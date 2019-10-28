1bbb9eb8e9b7423c61c4e83afc3b68ffea1d0705
Rijk van Putten <rijkvanputten@hotmail.nl>
2019-10-28 09:47:56 +0100

    TestContext & Texture test

--- src/org/djog_unos/tankgame/unittest/ShellTest.java
+++ src/org/djog_unos/tankgame/unittest/ShellTest.java
@@ -1,7 +1,6 @@
 package org.djog_unos.tankgame.unittest;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.djog_unos.tankgame.game.Shell;
 import org.joml.Vector2f;
--- src/org/djog_unos/tankgame/unittest/TestRunner.java
+++ src/org/djog_unos/tankgame/unittest/TestRunner.java
@@ -1,5 +1,9 @@
 package org.djog_unos.tankgame.unittest;
 
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+
 /*
 Using JUnit 4
  */
@@ -7,10 +11,14 @@ Using JUnit 4
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
+import org.lwjgl.opengl.GL;
 
 public class TestRunner {
     public static void main(String[] args) {
         System.out.println("\nRunning tests...");
+        
+        createTestContext();
+        
         Result result = JUnitCore.runClasses(
             ShellTest.class, 
             TextureTest.class
@@ -23,5 +31,16 @@ public class TestRunner {
         for (Failure failure : result.getFailures()) {
             System.out.println("Failure: " + failure.toString());
         }
+    }
+    
+    static void createTestContext()
+    { // Create an openGL context for testing
+    	if(!glfwInit()) {
+			System.err.println("Failed To Initialize!");
+			System.exit(1);
+    	}
+        long window = glfwCreateWindow(100, 100, "Test", 0, 0);
+        glfwMakeContextCurrent(window);
+        GL.createCapabilities();
     }
 }
