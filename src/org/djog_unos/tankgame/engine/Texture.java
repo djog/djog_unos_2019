6a931c0ad3df3ef73b9208e0f912c12ae0583f08
Rijk van Putten <rijkvanputten@hotmail.nl>
2019-10-28 17:33:54 +0100

    Basic UI

--- src/org/djog_unos/tankgame/engine/Texture.java
+++ src/org/djog_unos/tankgame/engine/Texture.java
@@ -13,25 +13,27 @@ import javax.imageio.ImageIO;
 import org.lwjgl.BufferUtils;
 
 public class Texture {
-    private int id;
+
+    private int m_id;
+    private int m_width, m_height;
 
     public Texture(String path) {
         BufferedImage bi;
         try {
             bi = ImageIO.read(new File(path));
-            int width = bi.getWidth();
-            int height = bi.getHeight();
+            m_width = bi.getWidth();
+            m_height = bi.getHeight();
 
-            int[] rawPixels = new int [width * height * 4];
-            rawPixels = bi.getRGB(0, 0, width, height, null, 0, width);
+            int[] rawPixels = new int [m_width * m_height * 4];
+            rawPixels = bi.getRGB(0, 0, m_width, m_height, null, 0, m_width);
 
-            ByteBuffer pixels = BufferUtils.createByteBuffer(width * height * 4);
+            ByteBuffer pixels = BufferUtils.createByteBuffer(m_width * m_height * 4);
 
-            for (int x = 0; x < width; x++)
+            for (int x = 0; x < m_width; x++)
             {
-                for (int y = 0; y < height; y++)
+                for (int y = 0; y < m_height; y++)
                 {
-                    int pixel = rawPixels[x * width + y];
+                    int pixel = rawPixels[x * m_width + y];
                     pixels.put((byte)((pixel >> 16) & 0xFF));   // RED
                     pixels.put((byte)((pixel >> 8) & 0xFF));    // GREEN
                     pixels.put((byte)(pixel & 0xFF));           // BLUE
@@ -41,14 +43,14 @@ public class Texture {
 
             pixels.flip();
 
-            id = glGenTextures();
+            m_id = glGenTextures();
             
-            glBindTexture(GL_TEXTURE_2D, id);
+            glBindTexture(GL_TEXTURE_2D, m_id);
 
             glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,  GL_NEAREST);
             glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,  GL_NEAREST);
 
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_width, m_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
         } 
         catch (IOException e)
         {
@@ -56,16 +58,33 @@ public class Texture {
         }
     }
 
+    Texture(int width, int height, ByteBuffer pixels)
+    {
+        m_id = glGenTextures();
+        this.m_width = width;
+        this.m_height = height;
+
+        glBindTexture(GL_TEXTURE_2D, m_id);
+
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,  GL_NEAREST);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,  GL_NEAREST);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+    }
+
     public void bind(int sampler)
     {
         if (sampler >= 0 && sampler <= 31)
         {
             glActiveTexture(GL_TEXTURE0 + sampler);
-            glBindTexture(GL_TEXTURE_2D, id);
+            glBindTexture(GL_TEXTURE_2D, m_id);
         }
         else
         {
             System.err.println("ERROR: The sampler must be in the range of 0 - 31!");
         }
     }
+
+    public int getWidth() { return m_width; }
+    public int getHeight() { return m_height; }
 }
--- src/org/djog_unos/tankgame/game/GameView.java
+++ src/org/djog_unos/tankgame/game/GameView.java
@@ -1,16 +1,16 @@
 package org.djog_unos.tankgame.game;
 
-import org.djog_unos.tankgame.engine.Sprite;
+import org.djog_unos.tankgame.engine.*;
 
 public class GameView  
 {
 	public static void drawGame(TankGame game)
 	{
 		game.background.draw();
-		game.player.draw();
 		game.box1.draw();
 		game.box2.draw();
 		game.box3.draw(); 
+		game.player.draw();
 
 		// Draw shells
         Sprite shellSprite = new Sprite("shell.png", 32, 32, 0);
@@ -19,6 +19,10 @@ public class GameView
             shellSprite.setPosition(shell.getX(), shell.getY());
             shellSprite.setRotation(shell.getAngle());
             shellSprite.draw();
-        }
+		}
+
+		Font font = new Font();
+		font.drawText("TEST", 10, 10);
+		
 	}
 }
