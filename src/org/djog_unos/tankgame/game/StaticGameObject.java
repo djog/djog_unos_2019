8d03c03e79175fb238285e50f8411f16a9a2c586
RafayelGardishyan <rgardishyan@gmail.com>
2019-11-09 18:37:24 +0100

    Added a simple change that will help duplicating code in the future

---
+++ src/org/djog_unos/tankgame/game/StaticGameObject.java
@@ -0,0 +1,27 @@
+package org.djog_unos.tankgame.game;
+
+import org.djog_unos.tankgame.engine.Sprite;
+
+abstract public class StaticGameObject {
+    private Sprite m_sprite;
+    private float m_x = 0.0f;
+    private float m_y = 0.0f;
+
+    public StaticGameObject(float x, float y) {
+        m_x = x;
+        m_y = y;
+    }
+
+    public void init(String textureName, int width, int height)
+    {
+        m_sprite = new Sprite(textureName, width, height, 0);
+        m_sprite.setPosition(m_x, m_y);
+    }
+
+    public void draw(){ m_sprite.draw();}
+
+    public float get_x() { return m_x; }
+    public float get_y() { return m_y; }
+    public float get_width() { return m_sprite.get_width(); }
+    public float get_height() { return m_sprite.get_height(); }
+}
